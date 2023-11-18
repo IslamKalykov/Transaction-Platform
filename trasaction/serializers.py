f23f271b235f87644647d79bb120ae425d29c263
IslamKalykov <islamkalykov1@gmail.com>
2023-11-18 15:38:22 +0600

    Serializers and views added

--- trasaction/serializers.py
+++ trasaction/serializers.py
@@ -1,9 +1,40 @@
-from .models import Transaction
 from rest_framework import serializers
-
+from .models import Transaction
 
 class TransactionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Transaction
         fields = '__all__'
 
+    def validate_amount(self, value):
+        """
+        Пользовательская валидация для поля 'amount'.
+        Убедитесь, что сумма транзакции больше 0.
+        """
+        if value <= 0:
+            raise serializers.ValidationError("Сумма транзакции должна быть больше 0.")
+        return value
+
+    def validate(self, data):
+        """
+        Общая валидация для проверки условий перед сохранением транзакции.
+        """
+        amount = data.get('amount', 0)
+        source_account = data.get('from_account')
+        destination_account = data.get('to_account')
+
+        if amount > source_account.balance:
+            raise serializers.ValidationError("Недостаточно средств на счете для проведения транзакции.")
+
+        if not source_account.is_active or source_account.is_blocked:
+            raise serializers.ValidationError("Счет отправителя неактивен или заблокирован.")
+        
+        if destination_account == source_account:
+            raise serializers.ValidationError("Такой перевод невозможен.")
+        
+        if source_account.balance - amount < source_account.min_balance:
+            raise serializers.ValidationError("Недостаточно средств на счете для проведения транзакции.")
+
+        print(amount)
+
+        return data
--- trasaction/views.py
+++ trasaction/views.py
@@ -1,10 +1,38 @@
-from django.shortcuts import render
 from rest_framework.generics import ListCreateAPIView
 from .models import Transaction
 from .serializers import TransactionSerializer
+from rest_framework.response import Response
+from rest_framework import status
 # Create your views here.
 
 
 class TransactionListView(ListCreateAPIView):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
+
+    def perform_create(self, serializer):
+        # Получаем данные из запроса
+        amount = serializer.validated_data.get('amount')
+        from_account = serializer.validated_data.get('from_account')
+        to_account = serializer.validated_data.get('to_account')
+
+        # Проверяем, достаточно ли средств на счете отправителя
+        if from_account.balance < amount:
+            # Если недостаточно средств, устанавливаем статус транзакции в "Неуспешно"
+            serializer.save(status="Неуспешно")
+            return Response({'error': 'Недостаточно средств на счете отправителя'},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        # Проводим транзакцию (уменьшаем баланс отправителя и увеличиваем баланс получателя)
+        from_account.balance -= amount
+        to_account.balance += amount
+        from_account.save()
+        to_account.save()
+
+        # Устанавливаем статус транзакции в "Успешно"
+        serializer.save(status="Успешно")
+        
+        # Можно добавить дополнительные поля или логику, если нужно
+
+        # Возвращаем успешный ответ
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
