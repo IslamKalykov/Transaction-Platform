f79bbaa7bcb5a5ff972eafa1e92951d5d9acd8f5
Dream-Team17 <scout31217@gmail.com>
2023-11-18 14:28:15 +0600

    link admin backed to urls.py

--- transactions/urls.py
+++ transactions/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path("api/v1/", include("trasaction.urls")),
     path("api/v1/", include("account.urls")),
     path("api/v1/", include("user.urls"))
