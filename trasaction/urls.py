53d062d310f9d83748c7c8d0de2fa3980008dc2b
Dream-Team17 <scout31217@gmail.com>
2023-11-18 16:32:22 +0600

    resolved conflicts

--- trasaction/urls.py
+++ trasaction/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import AccountTransactionView, AccountDetailView
+from .views import TransactionListView
 
 
 urlpatterns = [
-    path('transactions/', AccountTransactionView.as_view(), name='transaction_list'),
-    path('accounts/<int:pk>/', AccountDetailView.as_view(), name='account-detail')
+    path('transactions/', TransactionListView.as_view(), name='transaction_list'),
+    # path('accounts/<int:pk>/', AccountDetailView.as_view(), name='account-detail')
 ]
 
 
