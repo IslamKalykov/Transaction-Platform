a1917efb7542467adc4682415a910f42d8e015ee
Dream-Team17 <scout31217@gmail.com>
2023-11-18 14:14:11 +0600

    added transaction/urls to main urls.py

--- trasaction/serializers.py
+++ trasaction/serializers.py
@@ -1,9 +1,9 @@
-from models import Transaction
+from .models import Transaction
 from rest_framework import serializers
 
 
 class TransactionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Transaction
-        fields = 'all'
+        fields = '__all__'
 
--- trasaction/views.py
+++ trasaction/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from rest_framework.generics import ListCreateAPIView
+from .models import Transaction
+from .serializers import TransactionSerializer
 # Create your views here.
+
+
+class TransactionListView(ListCreateAPIView):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
