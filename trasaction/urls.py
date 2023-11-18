c2a6d2a965b4bf48308c007fc508e98ec5b30b8c
Dream-Team17 <scout31217@gmail.com>
2023-11-18 14:24:09 +0600

    fixed conflicts

--- trasaction/urls.py
+++ trasaction/urls.py
@@ -1,15 +1,9 @@
 from django.urls import path
-<<<<<<< HEAD
 from .views import TransactionListView
 
 
 urlpatterns = [
-    path('', TransactionListView.as_view(), name='transaction_list'),
-    # Другие URL-маршруты вашего приложения...
-=======
-
-from trasaction import views
-
-urlpatterns = [
->>>>>>> e3db745243e2bc4a054a632972fa1b53a993e0aa
+    path('transactions/', TransactionListView.as_view(), name='transaction_list')
 ]
+
+
